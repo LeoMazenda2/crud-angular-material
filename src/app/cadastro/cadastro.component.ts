c1f9992c9ae784b539258948b93ca7dbea64defa
lmazenda <leonildo.mazenda@xdsoftware.ao>
2025-05-14 11:45:14 +0100

    Notificaçoes toast

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -6,6 +6,7 @@ import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Cliente } from './cliente';
 import { ClienteService } from '../cliente.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -33,6 +34,7 @@ export class CadastroComponent implements OnInit {
 
   Cliente: Cliente = Cliente.newCliente();
   Actualizando: boolean = false;
+  private snack: MatSnackBar = inject(MatSnackBar);
 
   constructor(
     private serviceCliente: ClienteService,
@@ -59,10 +61,17 @@ export class CadastroComponent implements OnInit {
     if(!this.Actualizando){
       this.serviceCliente.salvar(this.Cliente);
       this.Cliente = Cliente.newCliente();
+      this.mostrarMensagem("Salvo com sucesso");
     }
     else{
       this.serviceCliente.actualiar(this.Cliente);
       this.router.navigate(['/consulta']);
+       this.mostrarMensagem("Actualixado com sucesso");
     }
   }
+
+  mostrarMensagem(mensagem: string){
+    this.snack.open(mensagem, "OK");
+  }
+  
 }
--- src/app/consulta/consulta.component.ts
+++ src/app/consulta/consulta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,  } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
@@ -6,6 +6,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ClienteService } from '../cliente.service';
 import { Cliente } from '../cadastro/cliente';
@@ -31,7 +32,7 @@ export class ConsultaComponent implements OnInit {
   nomeBusca: string = '';
   listaClientes: Cliente[] = [];
   colunasTable: string[] = ['id', 'nome', 'cpf', 'dataNascimento', 'email', 'acoes'];
-
+  private snack: MatSnackBar = inject(MatSnackBar);
 
   constructor(
     private service: ClienteService, 
@@ -58,6 +59,7 @@ export class ConsultaComponent implements OnInit {
 
   deletar(cliente: Cliente){
     this.service.deletar(cliente);
-    this.listaClientes = this.service.pesquisarClientes('')
+    this.listaClientes = this.service.pesquisarClientes('');
+    this.snack.open('Item deletado com sucesso!', "OK");
   }
 }
