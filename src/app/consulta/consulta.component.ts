5cd7f72f516968305df6962804ebd76bb40db93f
leonildo.mazenda <leonildo.mazenda@hotmail.com>
2025-05-13 20:45:38 -0700

    adicionado o botão pesquisar

--- src/app/consulta/consulta.component.ts
+++ src/app/consulta/consulta.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-consulta',
-  imports: [],
+  imports: [
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    FlexLayoutModule
+  ],
   templateUrl: './consulta.component.html',
   styleUrl: './consulta.component.scss'
 })
